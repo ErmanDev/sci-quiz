e8dda6f82702a149ac828915f856b6b0811680eb
ErmanDev <ermanfaminiano020@gmail.com>
2025-12-03 07:38:05 +0800

    new push

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,4 +1,4 @@
 export const REQUIRE_VERIFICATION =
   process.env.REQUIRE_VERIFICATION === 'true';
 
-export const API_URL = process.env.API_URL || process.env.VITE_API_URL || 'http://localhost:4000';
+export const API_URL = "https://sci-quiz.onrender.com";
